d8f0e6fbbead3a86e16d4cd6ce443289ead5882e
Hao Li <haoli@Haos-MacBook-Pro.local>
2020-05-05 22:08:57 -0400

    fixed CPU usage command

--- AzureDiagnostics2/src/AzureSSHClient.java
+++ AzureDiagnostics2/src/AzureSSHClient.java
@@ -14,104 +14,100 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 class AzureSSHClient {
-    private static final String REDIS_HOST = "40.87.94.171";
-    
-    private static final String command1 = "free -m | awk \'NR==2{printf \"Memory Usage: %s/%sMB (%.2f%%)\", $3,$2,$3*100/$2 }\' ";
-    private static final String command2 = "df -h | awk \'$NF==\"/\"{printf \"Disk Usage: %d/%dGB (%s)\", $3,$2,$5}\'";
-    private static final String command3 = "awk -v a=\"$(awk \'/cpu /{print $2+$4,$2+$4+$5}\' /proc/stat; sleep 1)\" \'/cpu /{split(a,b,\" \"); print 100*($2+$4-b[1])/($2+$4+$5-b[2])}\'  /proc/stat";
-    
-    private Jedis _jedis;
-    private List<String> _users;
-    private List<String> _commands;
-    private Map<String, List<String>> _userIdServerMap;
-    
-    public AzureSSHClient(List<String> users, Map<String, List<String>> userIdServerMap) throws IOException {
-    	this._userIdServerMap = userIdServerMap;
-    	this._users = users;
-    	this._jedis = new Jedis(REDIS_HOST);    	
-    	this._commands = Arrays.asList(command1, command2, command3);
-    }
-    
-    
-    public void startAzureSSHClient() throws JSchException, IOException, InterruptedException {   
-    	while (true) {
-        	for (String user: this._users) {
-        		System.out.println("Starting azure client for user: " + user);
-        		
-        		List<String> servers = this._userIdServerMap.get(user);
-        		
-        		for (String server: servers) {
-        			this.runCommand(user, server);
-        		}
-        	}    
-        	TimeUnit.SECONDS.sleep(5);
-    	}
-    }
+  private static final String REDIS_HOST = "40.87.94.171";
 
-    
-    private void runCommand(String user, String server) throws JSchException, IOException {
-        Session session = setupSshSession(user, server);
-        
-        try {
-        	session.connect();
-        } catch (Exception e) {
-        	System.out.println(e);
-        	System.out.println("Private Key may have been misconfigured");
-        	return;
-        }
+  private static final String command1 = "free -m | awk \'NR==2{printf \"Memory Usage: %s/%sMB (%.2f%%)\", $3,$2,$3*100/$2 }\' ";
+  private static final String command2 = "df -h | awk \'$NF==\"/\"{printf \"Disk Usage: %d/%dGB (%s)\", $3,$2,$5}\'";
+  private static final String command3 = "ps -A -o %cpu | awk \'{s+=$1} END {print s \"%\"}\'";
 
+  private Jedis _jedis;
+  private List<String> _users;
+  private List<String> _commands;
+  private Map<String, List<String>> _userIdServerMap;
 
-        boolean flag = true;
-        for (String command : this._commands) { 
-            Channel channel=session.openChannel("exec");
-            
-        	((ChannelExec)channel).setCommand(command);
-            channel.setInputStream(null);
-            ((ChannelExec)channel).setErrStream(System.err);
-            
-            InputStream in=channel.getInputStream();
-            channel.connect();
-            
-            byte[] tmp=new byte[1024];
-            while(flag){
-              while(in.available()>0){
-                int i=in.read(tmp, 0, 1024);
-                if(i<=0)break;
-                
-                String data = new String(tmp, 0, i);
-                this._jedis.publish(user, data);
-                System.out.println(data);
-                flag = false;
-              }
-            }
-            flag = true;
-            channel.disconnect();
-    }
-        session.disconnect();
+  public AzureSSHClient(List<String> users, Map<String, List<String>> userIdServerMap) throws IOException {
+    this._userIdServerMap = userIdServerMap;
+    this._users = users;
+    this._jedis = new Jedis(REDIS_HOST);
+    this._commands = Arrays.asList(command1, command2, command3);
+  }
 
-    }
-    
-    
-    private Session setupSshSession(String userName, String server) throws JSchException {
-    	try {
-    		System.out.println("Setting up session for: " + server);
-            JSch jsch=new JSch();
-           
-            
-            String privateKeyPath = "../../users/" + userName + "/keys/" + server + ".txt";
-      		jsch.addIdentity(privateKeyPath);
+  public void startAzureSSHClient() throws JSchException, IOException, InterruptedException {
+    while (true) {
+      for (String user : this._users) {
+        System.out.println("Starting azure client for user: " + user);
 
-      		String host = userName + "@" + server;
-      		String user = host.substring(0, host.indexOf('@'));
-      		host = host.substring(host.indexOf('@')+1);
-      		
-      		Session session = jsch.getSession(user, host, 22);
-      		session.setConfig("StrictHostKeyChecking", "no"); // disable check for RSA key
-      		return session;
-      		
-        } catch(Exception e){
-          System.out.println(e);
-          return null;
+        List<String> servers = this._userIdServerMap.get(user);
+
+        for (String server : servers) {
+          this.runCommand(user, server);
         }
       }
+      TimeUnit.SECONDS.sleep(5);
+    }
+  }
+
+  private void runCommand(String user, String server) throws JSchException, IOException {
+    Session session = setupSshSession(user, server);
+
+    try {
+      session.connect();
+    } catch (Exception e) {
+      System.out.println(e);
+      System.out.println("Private Key may have been misconfigured");
+      return;
+    }
+
+    boolean flag = true;
+    for (String command : this._commands) {
+      Channel channel = session.openChannel("exec");
+
+      ((ChannelExec) channel).setCommand(command);
+      channel.setInputStream(null);
+      ((ChannelExec) channel).setErrStream(System.err);
+
+      InputStream in = channel.getInputStream();
+      channel.connect();
+
+      byte[] tmp = new byte[1024];
+      while (flag) {
+        while (in.available() > 0) {
+          int i = in.read(tmp, 0, 1024);
+          if (i <= 0)
+            break;
+
+          String data = new String(tmp, 0, i);
+          this._jedis.publish(user, data);
+          System.out.println(data);
+          flag = false;
+        }
+      }
+      flag = true;
+      channel.disconnect();
+    }
+    session.disconnect();
+
+  }
+
+  private Session setupSshSession(String userName, String server) throws JSchException {
+    try {
+      System.out.println("Setting up session for: " + server);
+      JSch jsch = new JSch();
+
+      String privateKeyPath = "../../users/" + userName + "/keys/" + server + ".txt";
+      jsch.addIdentity(privateKeyPath);
+
+      String host = userName + "@" + server;
+      String user = host.substring(0, host.indexOf('@'));
+      host = host.substring(host.indexOf('@') + 1);
+
+      Session session = jsch.getSession(user, host, 22);
+      session.setConfig("StrictHostKeyChecking", "no"); // disable check for RSA key
+      return session;
+
+    } catch (Exception e) {
+      System.out.println(e);
+      return null;
+    }
+  }
 }
