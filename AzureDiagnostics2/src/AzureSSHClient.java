892e286de2212f478973343b6b7d97299de72293
frodr33 <fsr32@cornell.edu>
2020-05-05 20:39:11 -0400

    v5 jar to run program on redis server

--- AzureDiagnostics2/src/AzureSSHClient.java
+++ AzureDiagnostics2/src/AzureSSHClient.java
@@ -6,132 +6,63 @@ import com.jcraft.jsch.Session;
 
 import redis.clients.jedis.Jedis;
 
-import static java.util.Arrays.asList;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 class AzureSSHClient {
-	private static final String LOCAL_HOST = "127.0.0.1";
-    private static final String HAO_HOST_1 = "40.121.57.154";
-    private static final String HAO_HOST_2 = "40.121.233.18";
-    private static final String FRANK_HOST = "13.92.44.67";
     private static final String REDIS_HOST = "40.87.94.171";
     
-    private static final String HAO_PRIVATE_KEY = "/Users/haoli/.ssh/id_rsa";
-    private static final String FRANK_PRIVATE_KEY = "/Users/frankrodriguez/.ssh/id_rsa";
+    private static final String command1 = "free -m | awk \'NR==2{printf \"Memory Usage: %s/%sMB (%.2f%%)\", $3,$2,$3*100/$2 }\' ";
+    private static final String command2 = "df -h | awk \'$NF==\"/\"{printf \"Disk Usage: %d/%dGB (%s)\", $3,$2,$5}\'";
+    private static final String command3 = "awk -v a=\"$(awk \'/cpu /{print $2+$4,$2+$4+$5}\' /proc/stat; sleep 1)\" \'/cpu /{split(a,b,\" \"); print 100*($2+$4-b[1])/($2+$4+$5-b[2])}\'  /proc/stat";
     
-//    private static final String command1 = "free -m | awk \'NR==2{printf \"Memory Usage: %s/%sMB (%.2f%%)\", $3,$2,$3*100/$2 }\' ";
-//    private static final String command2 = "df -h | awk \'$NF==\"/\"{printf \"Disk Usage: %d/%dGB (%s)\", $3,$2,$5}\'";
-//    private static final String command3 = "awk -v a=\"$(awk \'/cpu /{print $2+$4,$2+$4+$5}\' /proc/stat; sleep 1)\" \'/cpu /{split(a,b,\" \"); print 100*($2+$4-b[1])/($2+$4+$5-b[2])}\'  /proc/stat";
-//    
+    private Jedis _jedis;
+    private List<String> _users;
+    private List<String> _commands;
+    private Map<String, List<String>> _userIdServerMap;
     
-    // Only work on Linux
-    private static final String command1 = "free -m | awk 'NR==2{printf \"Memory Usage: %s/%sMB (%.2f%%)\", $3,$2,$3*100/$2 }'";
-    private static final String command2 = "df -h | awk '$NF==\"/\"{printf \"Disk Usage: %d/%dGB (%s)\", $3,$2,$5}'";
-    
-    private List<String> servers;
-    private List<String> commands;
-    private String azureUsername;
-    private String privateKey;
-    private Jedis jedis;
-    
-    public AzureSSHClient(String user) {
-    	this.azureUsername = user;
-    	this.jedis = new Jedis(REDIS_HOST);
-    	
-    	if (user.equals("hl677")) {
-    		this.servers = Arrays.asList(HAO_HOST_1, HAO_HOST_2);
-    		this.privateKey = HAO_PRIVATE_KEY;
-    	}
-    	else if (user.equals("fsr32")) {
-    		this.servers = Arrays.asList(FRANK_HOST);
-    		this.privateKey = FRANK_PRIVATE_KEY;
-    	}
-    	
-    	this.commands = Arrays.asList(command1, command2);
-    }
-    
-    public void startSideCar() throws JSchException, IOException, InterruptedException {
-    	Session session = setupSshSession(FRANK_HOST);
-    	boolean flag = true;
-    	session.connect();
-    	String c1 = "git clone https://github.com/haoli98/Cloud-Diagnostics-Tool.git; cd Cloud-Diagnostics-Tool; mkdir hello; ls; python -u test.py &";
-    	
-        Channel channel=session.openChannel("exec");
-    	((ChannelExec)channel).setCommand(c1);
-        channel.setInputStream(null);
-        ((ChannelExec)channel).setErrStream(System.err);
-        
-        InputStream in=channel.getInputStream();
-        channel.connect();
-        
-        byte[] tmp=new byte[1024];
-        while(flag){
-          while(in.available()>0){
-            int i=in.read(tmp, 0, 1024);
-            if(i<=0)break;
-            
-            String data = new String(tmp, 0, i);
-            this.jedis.publish(this.azureUsername, data);
-            System.out.println(data);
-            flag = false;
-          }
-        }
-        flag = true;
-        channel.disconnect();
-        session.disconnect();
-    }
-    
-    public void getMetricsOnServer() throws JSchException, IOException {
-    	// Assuming on server, get metrics
-        String s;
-        Process p;
-                
-        for (String command: this.commands) {
-            try {
-                p = Runtime.getRuntime().exec(command);
-                BufferedReader br = new BufferedReader(
-                    new InputStreamReader(p.getInputStream()));
-                
-                while ((s = br.readLine()) != null)
-                    System.out.println("line: " + s);
-                
-                p.waitFor();
-                System.out.println ("exit: " + p.exitValue());
-                p.destroy();
-            } catch (Exception e) {}
-        }
+    public AzureSSHClient(List<String> users, Map<String, List<String>> userIdServerMap) throws IOException {
+    	this._userIdServerMap = userIdServerMap;
+    	this._users = users;
+    	this._jedis = new Jedis(REDIS_HOST);    	
+    	this._commands = Arrays.asList(command1, command2, command3);
     }
     
     
-    public void startAzureSSHClient() throws JSchException, IOException, InterruptedException {
-    	System.out.println("Starting azure client for user: " + this.azureUsername);
-    	
+    public void startAzureSSHClient() throws JSchException, IOException, InterruptedException {   
     	while (true) {
-    		for (String server: this.servers) {
-    			this.runCommand(server);	
-    		}
-    		
-    		// Collect metrics every 5 seconds
-    		TimeUnit.SECONDS.sleep(5);
+        	for (String user: this._users) {
+        		System.out.println("Starting azure client for user: " + user);
+        		
+        		List<String> servers = this._userIdServerMap.get(user);
+        		
+        		for (String server: servers) {
+        			this.runCommand(user, server);
+        		}
+        	}    
+        	TimeUnit.SECONDS.sleep(5);
     	}
     }
 
-    private void runCommand(String server) throws JSchException, IOException {
-        Session session = setupSshSession(server);
-        session.connect();
+    
+    private void runCommand(String user, String server) throws JSchException, IOException {
+        Session session = setupSshSession(user, server);
+        
+        try {
+        	session.connect();
+        } catch (Exception e) {
+        	System.out.println(e);
+        	System.out.println("Private Key may have been misconfigured");
+        	return;
+        }
+
 
         boolean flag = true;
-        for (String command : this.commands) { 
+        for (String command : this._commands) { 
             Channel channel=session.openChannel("exec");
             
         	((ChannelExec)channel).setCommand(command);
@@ -148,7 +79,7 @@ class AzureSSHClient {
                 if(i<=0)break;
                 
                 String data = new String(tmp, 0, i);
-                this.jedis.publish(this.azureUsername, data);
+                this._jedis.publish(user, data);
                 System.out.println(data);
                 flag = false;
               }
@@ -161,17 +92,20 @@ class AzureSSHClient {
     }
     
     
-    private Session setupSshSession(String server) throws JSchException {
+    private Session setupSshSession(String userName, String server) throws JSchException {
     	try {
+    		System.out.println("Setting up session for: " + server);
             JSch jsch=new JSch();
-      		jsch.addIdentity(this.privateKey);
+           
+            
+            String privateKeyPath = "../../users/" + userName + "/keys/" + server + ".txt";
+      		jsch.addIdentity(privateKeyPath);
 
-      		String host = this.azureUsername + "@" + server;
+      		String host = userName + "@" + server;
+      		String user = host.substring(0, host.indexOf('@'));
+      		host = host.substring(host.indexOf('@')+1);
       		
-      		String user=host.substring(0, host.indexOf('@'));
-      		host=host.substring(host.indexOf('@')+1);
-      		
-      		Session session=jsch.getSession(user, host, 22);
+      		Session session = jsch.getSession(user, host, 22);
       		session.setConfig("StrictHostKeyChecking", "no"); // disable check for RSA key
       		return session;
       		
--- AzureDiagnostics2/src/Main.java
+++ AzureDiagnostics2/src/Main.java
@@ -1,23 +1,110 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 
 import com.jcraft.jsch.JSchException;
 
 public class Main {
 
 	public static void main(String[] args) throws JSchException, IOException, InterruptedException {
-		
 		BufferedReader reader =  
                 new BufferedReader(new InputStreamReader(System.in)); 
 		
-		System.out.println("Enter netid (azure username): "); 
-        String user = reader.readLine(); 		
-		
-        AzureSSHClient client = new AzureSSHClient(user);
+		System.out.println("Do you want to add an ID or server to metric collection? (yes/no)"); 
+        String addUser = reader.readLine(); 
         
-        client.getMetricsOnServer();
-//        client.startSideCar();
-//        client.startAzureSSHClient();
+        if (addUser.equals("yes")) {
+    		System.out.println("Enter username:"); 
+            String userName = reader.readLine(); 	
+            
+            // Make directory
+            String path = "../../users/" + userName;
+            File f = new File(path);
+            if (f.mkdirs()) {
+            	System.out.println("created directory at: " + "/home/fsr32/src/users/" + userName);
+            } else {
+            	System.out.println("directory already exists");
+            }
+           
+            
+            System.out.println("Do you want to add a server for this username? (yes/no)");
+            String userServer = reader.readLine();
+            
+            while (userServer.equals("yes")) {
+            	System.out.println("What host address do you want to add?");
+            	String hostAddress = reader.readLine();
+            	
+            	// Save host address somewhere
+                try {
+                	// First create hosts.txt if doesn't exist
+                	FileWriter myWriter = new FileWriter(path + "/hosts.txt"); 
+                    myWriter.write(hostAddress);
+                    myWriter.close();
+                    
+                    // Create private keys directory if doesn't exist
+                    String privateKeyDirectoryPath = path + "/keys";
+                    new File(privateKeyDirectoryPath).mkdirs();
+                    
+                    // Create file for private key
+                    String privateKeyFilePath = privateKeyDirectoryPath + "/" + hostAddress + ".txt";
+                    File myObj = new File(privateKeyFilePath);
+                    
+                    if (myObj.createNewFile()) {
+                      String linuxAddress = "/home/fsr32/src/users/" + userName + "/keys/" + hostAddress + ".txt";
+                      System.out.println("File created for VM: " + hostAddress + ". Add private key to file at: " + linuxAddress);
+                    } else {
+                      System.out.println("File already exists.");
+                    }
+                    
+                    
+                  } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                  }    	
+                System.out.println("Do you want to add another server for this username? (yes/no)");
+                userServer = reader.readLine();
+            		
+            }
+        }
+        
+        // Get all users
+        File[] files = new File("../../users/").listFiles();
+        List<String> users = new ArrayList<String>();
+        
+        System.out.println("Running program for usernames: ");
+        for (File file : files) {
+            if (file.isDirectory()) {
+            	String userID = file.getName();
+            	System.out.println(userID);
+            	users.add(file.getName());
+            }
+        }
+        
+        // Get servers for each user
+        HashMap<String, List<String>> id_server_map = new HashMap<>();
+        for (String user : users) {
+        	List<String> servers = new ArrayList<String>();
+        	File hosts = new File("../../users/" +  user + "/hosts.txt"); 
+        	
+            Scanner myReader = new Scanner(hosts);
+            while (myReader.hasNextLine()) {
+              String data = myReader.nextLine();
+              servers.add(data);
+              System.out.println(data);
+            }
+        	
+            id_server_map.put(user, servers);
+            myReader.close();
+        }
+        
+         	
+        AzureSSHClient client = new AzureSSHClient(users, id_server_map);
+        client.startAzureSSHClient();
 	}
 }
